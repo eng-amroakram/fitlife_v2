c933bdfccda468e9bfe6522fccf9c48a70eaf514
eng-amroakram <eng.amroakram@gmail.com>
2023-07-24 21:25:34 +0300

    update repo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
 
     public function landing()
     {
-        return view('landing', ['title' => __('Landing')]);
+        return view('auth.login', ['title' => __('Login')]);
     }
 
     public function login()
